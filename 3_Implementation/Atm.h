abeabd92b39490f84fd531651b09bdffb8ee5888
sidarayteli <sidaray3650@gmail.com>
2022-02-11 12:39:50 +0530

    Added

--- 3_Implementation/Atm.h
+++ 3_Implementation/Atm.h
@@ -1,22 +1,16 @@
 /**
- * @file Atm.c
+ * @file atm.h
  * @author sidarayteli
  * @brief 
  * @version 0.1
- * @date 2022-02-10
+ * @date 2022-02-11
  * 
  * @copyright Copyright (c) 2022
  * 
  */
+#ifndef __ATM_H__
+#define __ATM_H__
 
-
-#include<stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
-#include"atm.h"
-
-//Functions
 int mainMenu(void);
 float checkBalance(float balance);
 float moneyDeposit(float balance);
@@ -24,3 +18,4 @@ float moneyWithdraw(float balance);
 int menuExit(void);
 int errorMessage(void);
 
+#endif
